9399783063643f98f1b9946ef9011c5977403bde
Ko-GyeongTae <kokt0203@naver.com>
2021-02-08 21:19:54 +0900

    for, range, ...args

--- main.go
+++ main.go
@@ -17,6 +17,18 @@ func repeatMe(words ...string){
 	fmt.Println((words))
 }
 
+func superAdd(numbers ...int) int {
+	total := 0
+	for i := 0; i< len(numbers); i++ {
+		fmt.Println(numbers[i])
+	}
+	for index, number := range numbers {
+		fmt.Println(index, number)
+		total += number
+	}
+	return total
+}
+
 func _lenAndUpper(name string) (_length int, _uppercase string) {
 	defer fmt.Println("I'm done")
 	_length = len(name)
@@ -32,4 +44,6 @@ func main() {
 	total, up := _lenAndUpper("kokt0203")
 	fmt.Println(total, up)
 	repeatMe("ko", "lee", "kang", "hyeon")
+	result := superAdd(1,2,3,4,5,6,7)
+	fmt.Println(result)
 }
