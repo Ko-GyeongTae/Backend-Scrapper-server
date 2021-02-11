613b9a7e15e5f2135a153bd5dfb073cdbb8b0223
Ko-GyeongTae <kokt0203@naver.com>
2021-02-11 13:41:40 +0900

    Add Method

--- main.go
+++ main.go
@@ -7,18 +7,18 @@ import (
 )
 
 func main() {
-	dictionary := mydict.Dictionary{"first":"First word"}
+	dictionary := mydict.Dictionary{}
 	word := "hello"
 	definition := "Greeting"
 	err := dictionary.Add(word, definition)
 	if err != nil{
 		fmt.Println(err)
 	}
-	def, err := dictionary.Search(word)
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println(def)
+	hello, _ := dictionary.Search(word)
+	fmt.Println("found", word, "definition :", hello)
+	err2 := dictionary.Add(word, definition)
+	if err2 != nil {
+		fmt.Println(err2)
 	}
 }
 
