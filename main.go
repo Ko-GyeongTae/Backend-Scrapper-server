30e10c88c5c4f24054f6b1b8b5911d413955aeec
Ko-GyeongTae <kokt0203@naver.com>
2021-02-11 19:00:39 +0900

    Go routines

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 func main(){
-	
+	go Count("ko")
+	go Count("KT")
+	Count("Kim")
+}
+
+func Count(person string) {
+	for i:=0;i<10;i++{
+		fmt.Println(person, "is good", i)
+		time.Sleep(time.Second)
+	}
 }
