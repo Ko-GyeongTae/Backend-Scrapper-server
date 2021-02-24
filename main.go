87e92157ce56dc403c1806434baac22530d58f1d
Ko-GyeongTae <kokt0203@naver.com>
2021-02-24 18:56:02 +0900

    Channels

--- main.go
+++ main.go
@@ -6,9 +6,18 @@ import (
 )
 
 func main(){
-	go Count("ko")
-	go Count("KT")
-	Count("Kim")
+	c := make(chan bool)
+	people := [2]string{"nico", "flynn"}
+	for _, person := range people {
+		go isSexy(person, c)
+	}
+	fmt.Println(<-c)
+	fmt.Println(<-c)
+}
+
+func isSexy(person string, c chan bool) {
+	time.Sleep(time.Second * 5)
+	c <- true
 }
 
 func Count(person string) {
