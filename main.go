bb9755677f5ce90513a3a813bef975356630393c
Ko-GyeongTae <kokt0203@naver.com>
2021-02-24 23:32:26 +0900

    Channels Recap

--- main.go
+++ main.go
@@ -6,23 +6,27 @@ import (
 )
 
 func main(){
-	c := make(chan bool)
-	people := [2]string{"nico", "flynn"}
+	c := make(chan string)
+	people := [5]string{"nico", "flynn", "dal", "japanguy", "larry"}
 	for _, person := range people {
 		go isSexy(person, c)
 	}
-	fmt.Println(<-c)
-	fmt.Println(<-c)
+	fmt.Println("Waiting for messages")
+	for i := 0; i< len(people); i++ {
+		fmt.Println("Received this message:", <-c)
+	}
 }
 
-func isSexy(person string, c chan bool) {
+func isSexy(person string, c chan string) {
 	time.Sleep(time.Second * 5)
-	c <- true
+	c <- person + " is sexy"
 }
 
+/*
 func Count(person string) {
 	for i:=0;i<10;i++{
 		fmt.Println(person, "is good", i)
 		time.Sleep(time.Second)
 	}
 }
+*/
