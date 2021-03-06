9343da86d0e7a69141f924ed413b4679b733b577
Ko-GyeongTae <kokt0203@naver.com>
2021-03-06 18:46:21 +0900

    Add: home.html

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "github.com/Ko-Gyeongtae/learngo/scrapper"
+import (
+	"net/http"
 
+	"github.com/Ko-Gyeongtae/learngo/scrapper"
+	"github.com/labstack/echo"
+)
+
+func handleHome(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, World")
+}
 func main(){
+	e := echo.New()
+	e.GET("/", handleHome)
+	e.Logger.Fatal(e.Start(":2000"))
 	scrapper.Scrape("term")
 }
