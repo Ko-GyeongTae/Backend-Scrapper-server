36de1177421b3a04f473cb209b334a8251a3524a
Ko-GyeongTae <kokt0203@naver.com>
2021-03-10 20:24:04 +0900

    FINISH

--- main.go
+++ main.go
@@ -8,15 +8,17 @@ import (
 	"github.com/labstack/echo"
 )
 
+const fileName string = "jobs.csv";
+
 func handleHome(c echo.Context) error { //홈화면 html 호출
 	return c.File("home.html")
 }
 
 func handleScrape(c echo.Context) error { //검색후 파일 다운로드
-	defer os.Remove("jobs.csv") //파일 삭제 
+	defer os.Remove(fileName) //파일 삭제 
 	term := strings.ToLower(scrapper.CleanString(c.FormValue("term")))
 	scrapper.Scrape(term) // 스크래핑
-	return c.Attachment("jobs.csv", "jobs.csv") //파일 다운로드
+	return c.Attachment(fileName, fileName) //파일 다운로드
 }
 
 func main(){
